1b17d4ecf672e08cce76e61150e4f7988ac46034
Ilyés Levente <levente.ilyes001@gmail.com>
2025-02-06 00:31:08 +0100

    header and svg

--- src/app/shared/components/product-card/product-card.component.ts
+++ src/app/shared/components/product-card/product-card.component.ts
@@ -1,12 +1,15 @@
 import { Component, Input, SimpleChanges, OnChanges } from '@angular/core';
 import { Product } from '../../models/product.model';
 import { CommonModule } from '@angular/common';
+import { NoImageSVGComponent } from '../../assets/icons/no-image-svg.component';
+import { CartSVGComponent } from '../../assets/icons/cart-svg.component';
 
 @Component({
   selector: 'app-product-card',
-  imports: [CommonModule],
+  imports: [CommonModule, NoImageSVGComponent, CartSVGComponent],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
+  standalone: true,
 })
 export class ProductCardComponent implements OnChanges {
   @Input() product!: Product;
